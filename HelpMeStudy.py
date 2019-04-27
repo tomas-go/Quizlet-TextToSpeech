4e35f8bad6ee580be65929ca050d1fb25c0d2c32
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-04-27 03:12:10 -0400

    Worked out how to get all the basic information for each item (name, number of terms, link). Did some documentation to clear some things up. Added a Subject.py file to move the class Subject code here. Will need to then import the information.

--- HelpMeStudy.py
+++ HelpMeStudy.py
@@ -3,19 +3,45 @@ from selenium import webdriver
 
 url = 'https://www.quizlet.com/tommyboy295'
 
+# Selenium request
 driver = webdriver.Firefox()
 driver.get(url)
-html = driver.execute_script("return document.documentElement.outerHTML")
+s_html = driver.execute_script("return document.documentElement.outerHTML")
 
-sel_soup = BeautifulSoup(html, 'html.parser')
+sel_soup = BeautifulSoup(s_html, 'html.parser')
 
-#titles = sel_soup.findAll(class_='DashboardListItem')
-#print(titles)
-for i in sel_soup.findAll(class_='DashboardListItem'):
-    text = i.findAll('span')
-    print(text)
-    for j in text:
-        print(j.get_text())
-    #set_name = text[1]
-    #set_num_of_terms = text[0]
-    #print("Name: " +set_name+ "\tNumber of Terms: " +set_num_of_terms)
+
+# Class for the flashcard groups. Gets there info so we can use this later based on the cards the user wants to study.
+class Subject:
+    def __init__(self, name, num_of_terms, link):
+        self.name = name
+        self.num_of_terms = num_of_terms
+        self.link = link
+
+    def info(self):
+        return "Group:" + self.name + "has " + self.num_of_terms + "terms."
+
+    def all_info(self):
+        print("Name: " + str(self.name) + "\nNumber of Terms: " + str(self.num_of_terms) + "\nLink: " + str(self.link))
+        print("---------------------------------")
+
+
+# Array to hold all the Subjects
+all_subjects = []
+
+# DashboardListItem is the section that holds all information regarding each flashcard group.
+# This loop pulls the name of the item, the number of terms associated with the item, and the link to the item.
+# It is then added to all_subjects as a new object with those values
+for item in sel_soup.findAll(class_='DashboardListItem'):
+    name = item.find('span', {'class': 'SetPreview-cardHeaderTitle'}).get_text()
+    number_of_terms = item.find('span', {'class': 'SetPreview-cardBylineTermsCount'}).get_text()
+    link = item.find('a', href=True)['href']
+    x = Subject(name, number_of_terms, link)
+    all_subjects.append(x)
+
+# For testing purposes. Prints all information regarding all subjects pulled from Quizlet user's page
+for i in range(len(all_subjects)):
+    all_subjects[i].all_info()
+
+# For testing purposes. Prints the number of subjects pulled.
+print(len(all_subjects))
