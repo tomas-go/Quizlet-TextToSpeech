ed0f3fbab4e0984021b1f20cae50c3fb808aa834
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-05-02 18:24:43 -0400

    Made the flashcard term the name of the text to speech mp3 file name. Added error checking for if the term is unusable as a file name and uses the term number (not index) instead.

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -12,6 +12,14 @@ def tts(all_terms, subject_name):
     for i in range(len(all_terms)):
         text = all_terms[i].term + ' ' + all_terms[i].definition
         speech = gTTS(text, 'en', 'slow')
-        speech.save('%d.mp3' % (i+1))
+
+        # default the file should be saved as mp3 with the file name being the term used.
+        try:
+            speech.save('%s.mp3' % all_terms[i].term)
+        # If term is unusable as a file name, just use the flashcard number
+        except (FileNotFoundError, OSError):
+            speech.save('%d.mp3' % (i + 1))
+
         print(str(i+1) + ') ' + text)
+
     print(subject_name + 'is done!')
