38a94c81b4c2c0df10d9dfdbe598922d1f4a3194
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-08-17 12:40:31 -0400

    Added listbox and was able to grab the subject names and list them in the gui.

--- GUI.py
+++ GUI.py
@@ -21,10 +21,26 @@ def get_page(entry):
     # BeautifulSoup parser
     sel_soup = BeautifulSoup(s_html, 'html.parser')
 
+    # Call to get all subject names
+    get_subject_info(sel_soup.findAll(class_='DashboardListItem'))
+
+def get_subject_info(subject_info):
+    for item in subject_info:
+        name = item.find('span', {'class': 'SetPreview-cardHeaderTitle'}).get_text()
+        number_of_terms = item.find('span', {'class': 'SetPreview-cardBylineTermsCount'}).get_text()
+        link = item.find('a', href=True)['href']
+        x = Subject(name, number_of_terms, link)
+        all_subjects.append(x)
+    populate_subject_names()
+
+def populate_subject_names():
+    for i in all_subjects:
+        subject_names.insert(all_subjects.index(i), i.name)
+
 
 # GUI 
-HEIGHT = 500
-WIDTH = 600
+HEIGHT = 750
+WIDTH = 900
 
 root = tk.Tk()
 root.title("Quizlet Text To Speech App")    # Title for window
@@ -32,7 +48,7 @@ root.title("Quizlet Text To Speech App")    # Title for window
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-top_frame = tk.Frame(root)
+top_frame = tk.Frame(root, bd=10)
 top_frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
 user_input = tk.Entry(top_frame, font=40)
@@ -43,5 +59,8 @@ submit_button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+subject_names = tk.Listbox(lower_frame)
+subject_names.place(relwidth=0.5, relheight=1)
 
 # root.mainloop()
