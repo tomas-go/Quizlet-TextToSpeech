cd984c3543b6029505d25a9775263cb5a8e00fb4
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-08-17 00:25:10 -0400

    Added a basic entry bar for the url and basic go submit button.

--- GUI.py
+++ GUI.py
@@ -8,5 +8,13 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+frame = tk.Frame(root)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+user_input = tk.Entry(frame, font=40)
+user_input.place(relwidth=0.65, relheight=1)
+
+submit_button = tk.Button(frame, text="Go", font="40")
+submit_button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 # root.mainloop()
